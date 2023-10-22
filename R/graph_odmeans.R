125e0254fa3568f07ddc69442fc93998845b5832
CrisHeredia00 <herediagalleguillos@gmail.com>
2023-10-22 11:40:58 -0300

    od_means_graph() function update. It uses a bbox and stamen_map. As stamen is deprecated, have to upgrade using stadia.

--- R/graph_odmeans.R
+++ R/graph_odmeans.R
@@ -1,55 +1,131 @@
-#ggplot
 
-od_means_graph <- function(odmeans_data, title="ODMeans Graph", borders=1.2, zoom=4, maptype=c("terrain-lines"), add_cluster=TRUE){
+coordinates
 
+custom_source <- list(
+  url = "AIzaSyDHnRGnUBfhwEjZ7l_X9G54ojjvgugFAYI",
+  zoom = 10,
+  extent = "custom",
+  bbox =c(left = -70.2, bottom =  -34.1, right = -70.2, top = -32.9 )
+)
+
+
+
+a = c(left = lowerleftlon, bottom = lowerleftlat, right = upperrightlon, top = upperrightlat )
+map = get_googlemap(bbox = a)
+
+ggmap(map)
+
+
+
+d_means_graph <- function(odmeans_data, title="ODMeans Graph", borders=0.2, zoom=4, maptype=c("roadmap"), add_cluster=TRUE){
+
+  # Obtain all information to graph
+  #odmeans_data = testing
   dfODMeans = data.frame(odmeans_data[["centers"]])
+  colnames(dfODMeans) <- c("OriginLatitude", "OriginLongitude", "DestinationLatitude", "DestinationLongitude")
+
   clusters = c(1:nrow(dfODMeans))
-  hierarchy = data.frame("hierarchy" = odmeans_data[["level_hierarchy"]])
-  dfODMeans = cbind(dfODMeans, clusters, hierarchy)
 
-  lowerleftlon = min(dfProbando["OriginLongitude"], dfProbando["DestinationLongitude"])*borders
-  upperrightlon = max(dfProbando["OriginLongitude"], dfProbando["DestinationLongitude"])*borders
-  lowerleftlat = min(dfProbando["OriginLatitude"], dfProbando["DestinationLatitude"])*borders
-  upperrightlat = max(dfProbando["OriginLatitude"], dfProbando["DestinationLatitude"])*borders
+  if (is.null(odmeans_data[["level_hierarchy"]]) != TRUE)
+    Hierarchy = data.frame("Hierarchy" = odmeans_data[["level_hierarchy"]])
+  else
+    Hierarchy = rep("no_hierarchy", nrow(dfODMeans))
 
-  coordinates = c(lowerleftlon, lowerleftlat, upperrightlon, upperrightlat)
+  dfODMeans = cbind(dfODMeans, clusters, Hierarchy)
+
+
+  # Coordinates of the map
+  lowerleftlon = min(dfODMeans["OriginLongitude"], dfODMeans["DestinationLongitude"])
+  upperrightlon = max(dfODMeans["OriginLongitude"], dfODMeans["DestinationLongitude"])
+  lowerleftlat = min(dfODMeans["OriginLatitude"], dfODMeans["DestinationLatitude"])
+  upperrightlat = max(dfODMeans["OriginLatitude"], dfODMeans["DestinationLatitude"])
+
+  lon_size = upperrightlon - lowerleftlon
+  lat_size = upperrightlat - lowerleftlat
+
+  lowerleftlon = lowerleftlon - lon_size*borders*sign(lon_size)
+  upperrightlon = upperrightlon +lon_size*borders*sign(lon_size)
+  lowerleftlat = lowerleftlat - lat_size*borders*sign(lat_size)
+  upperrightlat = upperrightlat + lat_size*borders*sign(lat_size)
+
+
+  location = c(left = lowerleftlon, bottom = upperrightlon, right = lowerleftlat, top = upperrightlat )
 
   map <- tryCatch(
     {
-      get_stamenmap(bbox = coordinates, zoom = zoom, maptype = maptype)
+      get_googlemap(location = location, zoom = zoom, maptype = maptype)
     },
     error = function(e) {
-      message("An error occurred while getting the map, try changing zoom or data.")
+      message("An error occurred while getting the map, try changing zoom, maptype or data.")
+      message(e)
       return(NULL)  # Return NULL or an appropriate value if there's an error
     }
   )
 
-  # Check if map is NULL (indicating an error) and handle it
+  # Check if map is NULL and return a NULL map
   if (is.null(map)) {
-    # You can add further error handling here or simply return NULL
     return(NULL)
   }
-  final_map = ggmap(map)+
-    geom_segment(data=dfODMeans,
-                 aes(y= OriginLatitude,
-                     yend= DestinationLatitude,
-                     x= OriginLongitude,
-                     xend= DestinationLongitude,
-                     color = hierarchy),
-                 linewidth= 1,
-                 arrow= arrow(length = unit(0.4, "cm"))) +
-    scale_fill_manual(aesthetics = "colour", values = c("Local" = "royalblue","Global" = "red")) +
-    geom_label_repel(data=dfODMeans, aes(label=as.integer(clusters), y=OriginLatitude, x=OriginLongitude), alpha= 0.7, max.overlaps = 26) +
-    theme_minimal() +
-    labs(title = paste("ODMeans Graph"))
+
+  ## Add cluster include number of clusters
+  if (add_cluster == TRUE)
+    final_map = ggmap(map)+
+      geom_segment(data=dfODMeans,
+                   aes(y= OriginLatitude,
+                       yend= DestinationLatitude,
+                       x= OriginLongitude,
+                       xend= DestinationLongitude,
+                       color = Hierarchy),
+                   linewidth= 1,
+                   arrow= arrow(length = unit(0.4, "cm"))) +
+      scale_fill_manual(aesthetics = "colour", values = c("Local" = "royalblue","Global" = "red")) +
+      geom_label_repel(data=dfODMeans,
+                       aes(label=as.integer(clusters), y=OriginLatitude, x=OriginLongitude),
+                       alpha= 0.7,
+                       max.overlaps = 26,
+                       na.rm=TRUE,
+                       xlim = c(lowerleftlon, upperrightlon),
+                       ylim = c (lowerleftlat, upperrightlat)) +
+      theme_minimal() +
+      labs(title = paste(title))
+  else {
+    final_map = ggmap(map)+
+      geom_segment(data=dfODMeans,
+                   aes(y= OriginLatitude,
+                       yend= DestinationLatitude,
+                       x= OriginLongitude,
+                       xend= DestinationLongitude,
+                       color = Hierarchy),
+                   linewidth= 1,
+                   arrow= arrow(length = unit(0.4, "cm"))) +
+      scale_fill_manual(aesthetics = "colour", values = c("Local" = "royalblue","Global" = "red")) +
+      theme_minimal() +
+      labs(title = paste(title))
+  }
 
   return(final_map)
-
 }
 
 
-# testing = od_means(ODMeansSampleData, 5, 1000, 2500, 500)
-# od_means_graph(testing) # zoom 6 y borders 1.2 error
+
+
+#function(data, numK, limitsSeparation, maxDist, distHierarchical)
+
+od_means_graph(testing_2, title="ODMeans Taxi Graph", borders=0.2, zoom=11, maptype=c("roadmap"), add_cluster=TRUE)
+
+
+testing = od_means(ODMeansTaxiData, 10, 30, 2200, 367)
+testing_2 = od_means(ODMeansTaxiData, 10, 30, 2200, 700)
+
+
+testing = od_means(ODMeansSampleData, 5, 1000, 2500, 500)
+
+od_means_graph(testing, borders=0.2, zoom=4, maptype=c("terrain-lines"), add_cluster=FALSE)
+
+od_means_graph(testing, borders=0.2, zoom=11, maptype=c("terrain-lines"), add_cluster=TRUE)
+
+
+testing_kmeans = kmeans(ODMeansTaxiData, 30)
 
 
 
